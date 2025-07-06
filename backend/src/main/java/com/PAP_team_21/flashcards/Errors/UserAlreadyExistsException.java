3e8343431c0986c84e885c688b939f8968e24250
mcieslik <01187214@pw.edu.pl>
2025-07-06 11:43:30 +0200

    deleted unnecessary files

--- backend/src/main/java/com/PAP_team_21/flashcards/Errors/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.PAP_team_21.flashcards.Errors;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/PAP_team_21/flashcards/entities/authority/AuthorityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.PAP_team_21.flashcards.entities.authority;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Integer> {
-}
